1ba932735daf225449a37003e3512fbb57c79def
Luciano Ortiz <luciano.ortiz@neogrid.com>
2022-11-15 18:17:22 -0300

    add lombok + slf4

--- src/main/java/com/lucianoortizsilva/ScheduleAmazon.java
+++ src/main/java/com/lucianoortizsilva/ScheduleAmazon.java
@@ -1,16 +1,23 @@
 package com.lucianoortizsilva;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@EnableAsync
 @Configuration
 @EnableScheduling
-public class Schedule {
+public class ScheduleAmazon {
 
+	@Async
 	@Scheduled(fixedRate = 10000)
-	public void x() {
-		System.out.println("Hello World!");
+	public void toProcess() {
+		log.info("----------AMAZON----------");
 	}
 
 }
